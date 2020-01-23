4ce922f4205ef9069b6c2f122a6623fe654c41ac
abauruel <abauruel@gmail.com>
2020-01-23 23:55:43 +0000

    added new component

---
+++ src/pages/Planos/Listagem/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdAdd } from 'react-icons/md';
+
+import { Container, ContainerHeader, ContainerList } from './styles';
+
+export default function Listagem() {
+  return (
+    <Container>
+      <ContainerHeader>
+        <div>
+          <p>Gerenciar planos</p>
+        </div>
+        <div>
+          <Link to="/cadastro de planos">
+            <MdAdd size={12} color="#FFF" />
+            <label>Cadastrar</label>
+          </Link>
+        </div>
+      </ContainerHeader>
+      <ContainerList>
+        <table>
+          <thead>
+            <tr>
+              <th>TITULO</th>
+              <th>DURAÇÃO</th>
+              <th>VALOR P/MÊS</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>name</td>
+              <td>email</td>
+              <td>age</td>
+              <td>
+                <button type="button">editar</button>
+              </td>
+
+              <td>
+                <button type="button">apagar</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </ContainerList>
+    </Container>
+  );
+}
---
+++ src/pages/Planos/Listagem/styles.js
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1200px;
+  margin: 20px auto;
+  padding: 0 30px;
+`;
+export const ContainerHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  font-family: Roboto, sans-serif;
+  div {
+    > p {
+      font-weight: bold;
+      font-size: 24px;
+    }
+  }
+  div {
+    a {
+      text-transform: uppercase;
+      font-weight: bold;
+      font-size: 14px;
+      border: none;
+      padding: 10px 30px;
+      background: #ee4d64;
+      color: #fff;
+      border-radius: 4px;
+      margin-right: 10px;
+
+      > label {
+        margin: 0 5px;
+      }
+    }
+    input {
+      padding: 10px 30px;
+      width: 200px;
+      border-radius: 4px;
+      border: 1px solid rgba(255, 255, 255, 0.06);
+      font-size: 14px;
+      ::placeholder {
+        font-size: 14px;
+        color: #9999;
+      }
+    }
+  }
+`;
+export const ContainerList = styled.div`
+  margin-top: 10px;
+  border-radius: 4px;
+  background: #fff;
+  padding: 10px;
+  table {
+    width: 100%;
+    font-family: Roboto, sans-serif;
+    font-size: 16px;
+
+    th,
+    td {
+      border-bottom: 1px solid #eee;
+      text-align: left;
+      padding: 10px;
+      :last-child {
+        button {
+          color: #de3b3b;
+        }
+      }
+      > button {
+        border: none;
+        font-size: 15px;
+        color: #4d85ee;
+      }
+    }
+  }
+`;
